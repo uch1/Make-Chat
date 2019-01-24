31be3eb6c7253ca67e90f43e4b7dafa2bda80eee
Uchenna Aguocha <uk.aguocha@gmail.com>
2019-01-24 07:19:28 -0800

    Integrated Socket.io

---
+++ server.js
@@ -0,0 +1,24 @@
+//app.js
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
+//Socket.io
+const io = require('socket.io')(server);
+io.on("connection", (socket) => {
+  console.log("🔌 New user connected! 🔌");
+})
+
+const exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs());
+app.set('view engine', 'pug');
+//Establish your public folder
+app.use('/public', express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+server.listen('3000', () => {
+  console.log('Server listening on Port 3000');
+})
