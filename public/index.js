84f0dc086491ed7d8b12b5e9174862bb890eb802
Uchenna Aguocha <uk.aguocha@gmail.com>
2019-01-24 22:47:51 -0800

    Implemented changing channles & Finished MakeChat tutorial

--- public/index.js
+++ public/index.js
@@ -1,9 +1,16 @@
 $(document).ready( () => {
-    //Connect to the socket.io server
+    //index.js
     const socket = io.connect();
-
-    // Keep track of the current user 
     let currentUser;
+    socket.emit('get online users');
+    //Each user should be in the general channel by default.
+    socket.emit('user changed channel', "General");
+
+    //Users can change the channel by clicking on its name.
+    $(document).on('click', '.channel', (e)=>{
+    let newChannel = e.target.textContent;
+    socket.emit('user changed channel', newChannel);
+    });
 
     $("#createUserBtn").click((e) => {
         e.preventDefault();
--- sockets/chat.js
+++ sockets/chat.js
@@ -58,5 +58,14 @@ module.exports = (io, socket, onlineUsers, channels) => {
         io.to(data.channel).emit('new message', data);
     })
 
+    //Have the socket join the room of the channel
+    socket.on('user changed channel', (newChannel) => {
+        socket.join(newChannel);
+        socket.emit('user changed channel', {
+        channel : newChannel,
+        messages : channels[newChannel]
+        });
+    })
+
 
 }
